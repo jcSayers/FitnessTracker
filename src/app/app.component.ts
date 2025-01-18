f39e32aab7859a99cf3822e3a5ac893efb451e4a
jcSayers <joshua.sayers@sudosky.com>
2025-01-18 13:10:47 +0200

    Trying to add angular material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { NgFor } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import {provideRouter, Router, RouterOutlet } from '@angular/router'
-
 import {MatButtonModule} from '@angular/material/button'
 @Component({
     selector: 'app-root',
--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {RouterOutlet } from '@angular/router'
 import { OnInit } from '@angular/core';
+
 @Component({
   selector: 'sign-in',
   templateUrl: './sign-in.component.html',
